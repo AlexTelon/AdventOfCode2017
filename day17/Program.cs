b26acac92ee0b6bcf65f3e38e9c92b0022bde238
Alex Telon <alex.telon@cybercom.com>
2017-12-17 14:44:45 +0100

    day17 star 1

---
+++ day17/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kattis
+{
+    class Program
+    {
+
+        class Board
+        {
+            public List<int> Data { get; set; } = new List<int>();
+            public int Step { get; set; }
+            public int Pos { get; set; } = 0;
+            private int counter = 0;
+
+            public void Next()
+            {
+                Pos = (Pos + Step) % Data.Count();
+                Data.Insert(Pos + 1, ++counter);
+                //Pos = (Pos + 1) % Data.Count();
+                Pos++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            var input = 3;
+            input = 369;
+            Board board = new Board() { Step = input };
+            board.Data.Add(0);
+
+            for (int i = 0; i < 2017; i++)
+            {
+                board.Next();
+                //Console.WriteLine(string.Join(" ", board.Data));
+            }
+
+            Console.WriteLine(string.Join(" ", board.Data));
+            Console.WriteLine(board.Pos);
+            Console.WriteLine(board.Data[board.Pos]);
+            Console.WriteLine(board.Data[board.Pos+1]);
+
+        }
+    }
+}
